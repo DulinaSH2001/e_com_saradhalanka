98f2a8c8bab2f58fe0ebcb72ddfb464a0f23db1a
DulinaSH2001 <dulinahejithaindrawansha@gmail.com>
2025-01-31 00:40:03 +0530

    Add demo JavaScript files and lazy image for demonstration

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/qb-apis/index.php/';
+    public string $baseURL = 'http://localhost/e_com_saradhalanka/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
